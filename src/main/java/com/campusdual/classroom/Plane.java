3b9c80e5158eaf7cdfca063585d3e2edbe06ff09
Miguel Ángel Rama Martínez <mianramar@gmail.com>
2024-10-01 10:42:48 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Plane.java
+++ src/main/java/com/campusdual/classroom/Plane.java
@@ -1,11 +1,26 @@
 package com.campusdual.classroom;
 
-public class Plane{
+public class Plane implements IMachine{
 
 	private final String name;
 
 	public Plane(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public void start(){
+		System.out.println("El avión " + name + "está encendido");
+	}
+
+	@Override
+	public void stop(){
+		System.out.println("El avión " + name + "está apagado");
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println("El avión " + name + "está en mantenimiento");
 	}
 
 	public void takeOff() {
--- src/main/java/com/campusdual/classroom/Tractor.java
+++ src/main/java/com/campusdual/classroom/Tractor.java
@@ -1,11 +1,26 @@
 package com.campusdual.classroom;
 
-public class Tractor{
+public class Tractor implements IMachine{
 
 	protected int horsePower = 0;
 	
 	public Tractor(int hp) {
 		this.horsePower = hp;
+	}
+
+	@Override
+	public void start(){
+		System.out.println("El tractor de " + horsePower + "de potencia está encendido");
+	}
+
+	@Override
+	public void stop(){
+		System.out.println("El tractor de " + horsePower + "de potencia está apagado");
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println("El tractor de " + horsePower + "de potencia está en mantenimiento");
 	}
 
 	public void forward() {
